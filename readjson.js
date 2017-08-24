d62191501b33ff6b28cb45411db30e2a6701d3ee
Mauricio Andrades <mod.and.op@gmail.com>
2017-08-23 23:31:29 -0700

    removed cached

--- readjson.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs');
-var json = JSON.parse(fs.readFileSync('/Users/Op/Documents/DEV/server/code-files/node/sinister.proc.json', 'utf8'))
-var requireg = require("/Users/Op/.npm-global/lib/node_modules/requireg/lib/requireg.js");
-var lodash = requireg('lodash');
-
