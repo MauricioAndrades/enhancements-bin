07a3a0a33f84d7289289a949fdac7cd4cf9b65df
Mauricio Andrades <mod.and.op@gmail.com>
2017-08-30 12:06:15 -0700

    better file names

--- tampermonkey/enhancements.js
+++ tampermonkey/enhancements.js
@@ -73,10 +73,15 @@
     if (typeof features.conditionCheck === "undefined") {features.conditionCheck = {}; features.conditionCheck.name = "Condition Check"; features.conditionCheck.enabled = featuresOptIn; }
     if (typeof features.addBulkDataSources === "undefined") {features.addBulkDataSources = {}; features.addBulkDataSources.name = "Add Bulk DataSources"; features.addBulkDataSources.enabled = featuresOptIn; }
     if (typeof features.updateTitle === "undefined") {features.updateTitle = {}; features.updateTitle.name = "Update TiQ Title"; features.updateTitle.enabled = featuresOptIn; }
-    if (typeof features.solutions_fix_conditions === "undefined") {features.solutions_fix_conditions = {};features.solutions_fix_conditions.url = "https://cdn.rawgit.com/MauricioAndrades/enhancements-bin/master/tampermonkey/mau5.utui.fix.conditions.js";features.solutions_fix_conditions.name = "Fix Incomplete Conditions";features.solutions_fix_conditions.enabled = featuresOptIn;}
+    if (typeof features.solutions_fix_conditions === "undefined") {
+      features.solutions_fix_conditions = {};
+      features.solutions_fix_conditions.url = "https://cdn.rawgit.com/MauricioAndrades/enhancements-bin/master/tampermonkey/mau5.utui.fix.conditions.js";
+      features.solutions_fix_conditions.name = "Fix Incomplete Conditions";
+      features.solutions_fix_conditions.enabled = featuresOptIn;
+    }
     if (typeof features.solutions_extra_info === "undefined") {
       features.solutions_extra_info = {};
-      features.solutions_extra_info.url = "https://rawgit.com/MauricioAndrades/enhancements-bin/master/tiq.qtip.config.js";
+      features.solutions_extra_info.url = "https://cdn.rawgit.com/MauricioAndrades/enhancements-bin/master/tampermonkey/tiq.extra.info.js";
       features.solutions_extra_info.name = "Solutions: Extra Info";
       features.solutions_extra_info.enabled = featuresOptIn;
     }
